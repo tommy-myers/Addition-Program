dae3a4377b875ab82d8d87ea64f22565c3d8d95a
Learn Python Tutorial <support@learnpythontutorial.com>
2015-03-16 11:14:31 -0400

    Addition Program in Python
    This is a basic addition program in Python

---
+++ addition.py
@@ -0,0 +1,5 @@
+print("Welcome to our addition program")
+firstnum = int(input("Give us your first number. "))
+secondnum = int(input("Give us your second number. "))
+thesum = firstnum + secondnum
+print("Your sum is {}".format(thesum))
